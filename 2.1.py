c1bc176a02df9e23d84365918f9de09ccb39b8d8
Hodinhhuan-k2 <hohuan862@gmail.com>
2024-11-29 13:21:30 +0700

    Delete 2.1.py

--- 2.1.py
+++
@@ -1,9 +0,0 @@
-print("Hồ Đình Huân")
-print("MSSV:235752021610049")
-
-n1=int(input("enter n1 value")) 
-n2=int(input("enter n2 value")) 
-if n1>n2: 
- print("n1 is big") 
-else: 
- print("n2 is big") 
